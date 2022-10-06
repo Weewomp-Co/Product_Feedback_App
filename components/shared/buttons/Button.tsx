44ca3933141ad1d02b8ee873461fa84af68059ca
weewomp1 <103136665+weewomp1@users.noreply.github.com>
2022-10-06 16:40:08 -0400

    v.102
    - made it all one file
    
    - BackButton1 and BackButton2 have been renamed to button5 and button6.
    
    - replaced fixed height/widths with padding and max/min widths/heights.
    
    - removed hover state color css variables from stitches config.

--- components/shared/buttons/Button.tsx
+++ components/shared/buttons/Button.tsx
@@ -1,8 +1,11 @@
 import { styled } from '../../../stitches.config'
 
 const Button1 = styled('button', {
-	width: "9.875em",
-	height: "2.75em",
+	maxWidth: "12em",
+	maxHeight: "2.75em",
+	minWidth: "142px",
+	minHeight: "38px",
+	padding: ".65em 3.025em",
 	fontFamily: '$jost', 
 	fontSize: '$h4',
 	backgroundColor: '$purple',
@@ -13,13 +16,19 @@ const Button1 = styled('button', {
 	transition: "all 120ms ease-out",
 	cursor: 'pointer',
 	'&:hover': {
-		backgroundColor: '$button1Hover'
-	}
+		backgroundColor: '#C75AF6'
+	},
+	userSelect: 'none',
+	wordWrap: "nowrap",
+	wordBreak: "keep-all"
 })
 
 const Button2 = styled('button', {
-	width: "9.875em",
-	height: "2.75em",
+	maxWidth: "12em",
+	maxHeight: "2.75em",
+	minWidth: "142px",
+	minHeight: "38px",
+	padding: ".65em 3.025em",
 	fontFamily: '$jost', 
 	fontSize: '$h4',
 	backgroundColor: '$blue600',
@@ -30,16 +39,22 @@ const Button2 = styled('button', {
 	transition: "all 120ms ease-out",
 	cursor: 'pointer',
 	'&:hover': {
-		backgroundColor: '$button2Hover'
-	}
+		backgroundColor: '#7C91F9'
+	},
+	userSelect: 'none',
+	wordWrap: "nowrap",
+	wordBreak: "keep-all"
 })
 
 const Button3 = styled('button', {
-	width: "9.875em",
-	height: "2.75em",
+	maxWidth: "12em",
+	maxHeight: "2.75em",
+	minWidth: "142px",
+	minHeight: "38px",
+	padding: ".65em 3.025em",
 	fontFamily: '$jost', 
 	fontSize: '$h4',
-	backgroundColor: '$blue300',
+	backgroundColor: '$grey600',
 	color: "$white300",
 	fontWeight: '$bold',
 	border: 'none',
@@ -47,13 +62,19 @@ const Button3 = styled('button', {
 	transition: "all 120ms ease-out",
 	cursor: 'pointer',
 	'&:hover': {
-		backgroundColor: '$button3Hover'
-	}
+		backgroundColor: '#656EA3'
+	},
+	userSelect: 'none',
+	wordWrap: "nowrap",
+	wordBreak: "keep-all"
 })
 
 const Button4 = styled('button', {
-	width: "9.875em",
-	height: "2.75em",
+	maxWidth: "12em",
+	maxHeight: "2.75em",
+	minWidth: "142px",
+	minHeight: "38px",
+	padding: ".65em 3.025em",
 	fontFamily: '$jost', 
 	fontSize: '$h4',
 	backgroundColor: '$red',
@@ -64,9 +85,58 @@ const Button4 = styled('button', {
 	transition: "all 120ms ease-out",
 	cursor: 'pointer',
 	'&:hover': {
-		backgroundColor: '$button4Hover',
+		backgroundColor: '#E98888',
 		color: "transparent"
-	}
+	},
+	userSelect: 'none',
+	wordWrap: "nowrap",
+	wordBreak: "keep-all"
 })
 
-export {Button1, Button2, Button3, Button4}
+const Button5 = styled('button', {
+  maxWidth: "10em",
+	maxHeight: "3.3125em",
+	minWidth: "140px",
+	minHeight: "3.3125em",
+	padding: ".942em 2.65em",
+	fontFamily: '$jost', 
+	fontSize: '$h4',
+	backgroundColor: 'transparent',
+	color: "$grey300",
+	fontWeight: '$bold',
+	border: 'none',
+	borderRadius: '10px',
+	transition: "all 120ms ease-out",
+	cursor: 'pointer',
+	'&:hover': {
+		textDecoration: "underline"
+	},
+	userSelect: 'none',
+	wordWrap: "nowrap",
+	wordBreak: "keep-all"
+})
+
+const Button6 = styled('button', {
+	maxWidth: "10em",
+	maxHeight: "3.3125em",
+	minWidth: "140px",
+	minHeight: "3.3125em",
+	padding: ".942em 2.65em",
+	fontFamily: '$jost', 
+	fontSize: '$h4',
+	backgroundColor: '#373F68',
+	color: "$white300",
+	fontWeight: '$bold',
+	border: 'none',
+	borderRadius: '10px',
+	transition: "all 120ms ease-out",
+	cursor: 'pointer',
+	'&:hover': {
+		textDecoration: "underline"
+	},
+	userSelect: 'none',
+	wordWrap: "nowrap",
+	wordBreak: "keep-all"
+})
+
+export {Button1, Button2, Button3, Button4, Button5, Button6}
--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -1,50 +1,9 @@
 import type { NextPage } from "next";
-import { styled } from '@stitches/react';
-import {Button1, Button2, Button3, Button4} from '../../components/shared/buttons/Button.tsx'
-import { BackButton1, BackButton2 } from "@/components/shared/buttons/BackButton";
-import { backArrow1, backArrow2 } from "../../assets/assets";
-import Image from "next/image";
-
-// const Button0 = styled('button', {
-// 	backgroundColor:  
-// })
 
 const Page: NextPage = () => {
 	return <div>
 		<h1>Hello world</h1>
 		
-		<div className="buttonsWrapper" style={{
-				display: "flex",
-				justifyContent: "center",
-				alignItems: "center",
-				gap: "8em",
-				flexDirection: "column",
-				height: "50vh"
-			}}>
-
-			<div className="buttonsShowcase" style={{
-				display: "flex",
-				justifyContent: "center",
-				alignItems: "center",
-				gap: "2em"
-			}}>
-			<Button1>Button 1</Button1>
-			<Button2>Button 2</Button2>
-			<Button3>Button 3</Button3>
-			<Button4>Button 4</Button4>
-			</div>
-
-			
-			<div className="backButtonShowcase" style={{
-				display: "flex",
-				justifyContent: "center",
-				alignItems: "center",
-				gap: "2em"
-			}}>
-				<BackButton1><Image src={backArrow1}/> Go Back</BackButton1>
-				<BackButton2><Image src={backArrow2}/> Go Back</BackButton2>
-			</div>
-		</div>
 	</div>;
 };
 
