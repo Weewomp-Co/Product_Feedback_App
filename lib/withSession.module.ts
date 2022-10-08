8ea8a7a38d34ef191e3b6f2c0456a42ad0b5719b
Nathan <39814328+Yofou@users.noreply.github.com>
2022-10-08 22:29:25 +0100

    Pro-15: signup and signin endpoints (#4)
    * chore(PRO-19): Setup docker-compose with nextjs
    
    * fix(PRO-19): Rename editorconfig to be correct lmao
    
    * chore(PRO-19): Add postgres image to docker-compose config
    
    * chore(PRO-19): Update readme
    
    * chore(PRO-15): (not functional commit) saving work
    
    * Squashed commit of the following:
    
    commit 4e30e2045fd91d32fa2a74d02f8ec20c3e05f060
    Author: Yofou <exodias1965@gmail.com>
    Date:   Sat Oct 8 15:58:17 2022 +0100
    
        chore(PRO-19): Update compose to just be postgres
    
    * chore(PRO-15): Basic strcuture update for (auth) backend
    
    * chore(PRO-15): Added Sign in, sign out & sign up
    
    * fix(PRO-15): santize to sanitize

---
+++ lib/withSession.module.ts
@@ -0,0 +1,26 @@
+import { withIronSessionApiRoute, withIronSessionSsr } from "iron-session/next";
+import { NextApiHandler } from "next";
+import { sanitizeUser } from "@/lib/user.module";
+
+declare module "iron-session" {
+  interface IronSessionData {
+    user?: ReturnType<typeof sanitizeUser>;
+  }
+}
+
+const sessionOptions = {
+  password: "25@SNC6dLvmCJkIl%&2lY116P*NwwRv!",
+  cookieName: "token",
+  // secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
+  cookieOptions: {
+    secure: process.env.NODE_ENV === "production",
+  },
+};
+
+export function withSessionRoute<T extends any>(handler: NextApiHandler<T>) {
+  return withIronSessionApiRoute(handler, sessionOptions);
+}
+
+export function withSessionSsr(handler: any) {
+  return withIronSessionSsr(handler, sessionOptions);
+}
