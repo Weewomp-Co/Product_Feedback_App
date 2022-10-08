0935d894c20abe404aad2cca5b15f3f17c665afd
Yofou <exodias1965@gmail.com>
2022-10-08 19:54:11 +0100

    chore(PRO-15): Added Sign in, sign out & sign up

--- lib/user.module.ts
+++ lib/user.module.ts
@@ -1,4 +1,7 @@
 import { User } from "@prisma/client";
+import { client } from "@/prisma/client"
+import { hash } from "argon2"
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 
 export const santizeUser = (user: User) => {
   return {
@@ -8,6 +11,21 @@ export const santizeUser = (user: User) => {
   }
 }
 
-export const createUser = () => {
+type CreateUser = Omit<User, "id">
+export const createUser = async (user: CreateUser) => {
+  return client.user.create({
+    data: {
+      email: user.email,
+      username: user.username,
+      password: await hash(user.password),
+    } 
+  })
+}
 
+export const handlePrismaUserError = (err: PrismaClientKnownRequestError) => {
+  if (err.code === "P2002") {
+    const target = err.meta?.target as string[] | undefined
+    if (!target?.[0]) return { '_errors': ["Unknown unqiue field contraint error"] }
+    return { [target?.[0]]: `User with that ${target?.[0]} already exists` }
+  }
 }
--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -1,11 +1,41 @@
+import { createUser, handlePrismaUserError, santizeUser } from "@/lib/user.module";
 import { withSessionRoute } from "@/lib/withSession.module";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 import { NextApiHandler } from "next";
+import { z } from "zod"
 
-const POST: NextApiHandler<any> = (req, res) => {
+const ValidatePostInput = z.object({
+  username: z.string().max(8),
+  email: z.string().email(),
+  password: z.string().min(8),
+  confirm_password: z.string(),
+}).refine((input) => input.password === input.confirm_password, {
+  message: "Passwords don't match",
+  path: ["confirm_password"], // path of error
+})
+
+const POST: NextApiHandler<any> = async (req, res) => {
+  const valBody = ValidatePostInput.safeParse(req.body)
+
+  if (valBody.success) {
+    try {
+      const user = await createUser(valBody.data)
+      req.session.user = santizeUser(user)
+      await req.session.save()
+
+      return res.json(req.session.user)
+    } catch(err) {
+      if (err instanceof PrismaClientKnownRequestError) {
+        return res.json(handlePrismaUserError(err))
+      }
+    }
+  } else {
+    res.json(valBody.error.format())
+  }
 }
 
 export default withSessionRoute((req, res) => {
   if (req.method === "POST") {
-    return POST(req, res)
+    POST(req, res)
   }
 })
