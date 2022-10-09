8432e40d02fa503d10cfc2c3ff00ffc6c80d3a55
weewomp1 <103136665+weewomp1@users.noreply.github.com>
2022-10-09 16:05:17 -0400

    sign update

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -1,10 +1,30 @@
 import type { NextPage } from "next";
-import AuthContainer from "@/components/shared/layouts/AuthContainer";
+import Votes from "@/components/shared/votes/Votes";
+import Input from "../../components/shared/input/Input";
+import { useState } from "react";
 
 const Page: NextPage = () => {
+  const [value, setValue] = useState("");
+  const [votes, setVotes] = useState(0);
+  const [hasClicked, setHasClicked] = useState(false);
+
   return (
     <div>
-      <AuthContainer title="Login"></AuthContainer>
+      <h1>Hello world</h1>
+      <Input
+        isError={false}
+        setValue={setValue}
+        value={value}
+        errorMessage={"Invalid input"}
+        type="text"
+        placeHolder="username"
+      />
+      <Votes 
+        votes={votes} 
+        setVotes={setVotes}
+        active={hasClicked} 
+        setActive={setHasClicked}
+      />
     </div>
   );
 };
