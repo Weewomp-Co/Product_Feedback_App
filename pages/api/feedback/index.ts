dccddb3a5c0398998e957c3f345adfefbf548015
Yofou <exodias1965@gmail.com>
2022-10-09 03:25:41 +0100

    chore(PRO-20): Added GET all feedback endpoint

--- pages/api/feedback/index.ts
+++ pages/api/feedback/index.ts
@@ -1,10 +1,24 @@
 import { withSessionRoute } from "@/lib/withSession.module";
 import { NextApiHandler } from "next";
+import { client } from "@/prisma/client"
 
 const POST: NextApiHandler<any> = (req, res) => {
 }
 
-const GET: NextApiHandler<any> = (req, res) => {
+const GET: NextApiHandler<any> = async (req, res) => {
+  const results = await client.feedback.findMany({
+    include: {
+      user: {
+        select: {
+          username: true,
+          email: true,
+          id: true
+        }
+      }
+    }
+  })
+
+  return res.json(results)
 }
 
 export default withSessionRoute((req, res) => {
