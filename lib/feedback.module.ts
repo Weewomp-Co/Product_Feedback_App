b569f7d47518371d397d1c426763bf0d7fea121e
Nathan <39814328+Yofou@users.noreply.github.com>
2022-10-09 18:18:48 +0100

    Chore(Pro-20): feedback basic (#7)
    * chore(PRO-20): Create feedback model (not working)
    
    * chore(PRO-20): Create feedback model (working) + endpoint blueprints
    
    * fix(PRO-20): Change post to put
    
    * chore(PRO-20): Added GET all feedback endpoint
    
    * chore(PRO-20): Added Get one, Create one, Update one, Delete one endpoints

---
+++ lib/feedback.module.ts
@@ -0,0 +1,40 @@
+import { client } from "@/prisma/client"
+import { Category, Feedback, Status } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+import { z } from "zod";
+
+export const GetOneFeedback = async (req: NextApiRequest) => {
+  const result = await client.feedback.findFirst({
+    where: {
+      id: req.query.uuid as string,
+    },
+  });
+
+  if (!result) {
+    return { 
+      _errors: [`Feedback not found by id of ${req.query.uuid}`] 
+    }
+  }
+
+  return result;
+}
+
+export const HasSessionCreatedFeedback = (feedback: Feedback, req: NextApiRequest, res: NextApiResponse<unknown>, keyword: string) => {
+  if (feedback.userId !== req.session.user?.id) {
+    res.status(401).json({
+      _errors: [`Unauthorized cannot ${keyword} another users feedback post`]
+    })
+
+    return true
+  }
+
+  return false
+}
+
+export const ValidateFeedbackBody = z.object({
+  title: z.string().min(5).max(25),
+  details: z.string().min(25).max(256),
+  status: z.enum([Status.Live, Status.Planned, Status.Progress, Status.Suggestion] as const).optional(),
+  category: z.enum([Category.UI, Category.UX, Category.Bug, Category.Feature, Category.Enchancement] as const),
+})
+
---
+++ pages/api/feedback/index.ts
@@ -0,0 +1,55 @@
+import { withSessionRoute } from "@/lib/withSession.module";
+import { NextApiHandler } from "next";
+import { Status } from "@prisma/client";
+import { client } from "@/prisma/client"
+import { ValidateFeedbackBody } from "@/lib/feedback.module";
+
+const POST: NextApiHandler<any> = async (req, res) => {
+  const parsedBody = ValidateFeedbackBody.safeParse(req.body)
+  if (parsedBody.success) {
+    const result = await client.feedback.create({
+      data: {
+        title: parsedBody.data.title,
+        details: parsedBody.data.details,
+        status: parsedBody.data.status ?? Status.Suggestion,
+        category: parsedBody.data.category,
+        userId: req.session.user?.id as string
+      },
+      include: {
+        user: {
+          select: {
+            id: true,
+            email: true,
+            username: true,
+          }
+        }
+      }
+    })
+
+    return res.status(201).json(result)
+  } else {
+    return res.json(parsedBody.error.format())
+  }
+}
+
+const GET: NextApiHandler<any> = async (_, res) => {
+  const results = await client.feedback.findMany({
+    include: {
+      user: {
+        select: {
+          username: true,
+          email: true,
+          id: true
+        }
+      }
+    }
+  })
+
+  return res.json(results)
+}
+
+export default withSessionRoute((req, res) => {
+  if (!req.session.user) return res.status(401).json({ unauthorized: false })
+  if (req.method === "POST") return POST(req, res) 
+  if (req.method === "GET") return GET(req, res)
+})
