30b131f685208a14910fe8ca82b1b565520c03e0
Yofou <exodias1965@gmail.com>
2022-10-09 03:20:25 +0100

    fix(PRO-20): Change post to put

--- pages/api/feedback/[uuid]/index.ts
+++ pages/api/feedback/[uuid]/index.ts
@@ -4,7 +4,7 @@ import { NextApiHandler } from "next";
 const GET: NextApiHandler<any> = (req, res) => {
 }
 
-const POST: NextApiHandler<any> = (req, res) => {
+const PUT: NextApiHandler<any> = (req, res) => {
 }
 
 const DELETE: NextApiHandler<any> = (req, res) => {
@@ -12,6 +12,6 @@ const DELETE: NextApiHandler<any> = (req, res) => {
 
 export default withSessionRoute((req, res) => {
   if (req.method === "GET") return GET(req, res)
-  if (req.method === "POST") return POST(req, res)
+  if (req.method === "PUT") return PUT(req, res)
   if (req.method === "DELETE") return DELETE(req, res)
 })
