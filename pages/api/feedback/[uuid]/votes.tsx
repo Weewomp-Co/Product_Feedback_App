e369beb6d4d2e3966df7cc5340d5dbeaa0ff6409
Yofou <exodias1965@gmail.com>
2022-10-09 19:47:57 +0100

    chore(PRO-26): Feedback vote toggle endpoint

--- pages/api/feedback/[uuid]/votes.tsx
+++ pages/api/feedback/[uuid]/votes.tsx
@@ -1,9 +1,53 @@
 import { withSessionRoute } from "@/lib/withSession.module";
 import { NextApiHandler } from "next";
+import { client } from "@/prisma/client"
+import { z } from "zod";
 
-const POST: NextApiHandler<any> = (req, res) => {};
+const POST: NextApiHandler<any> = async (req, res) => {
+  const postExist = await client.feedback.findFirst({
+    where: {
+      id: req.query.uuid as string
+    }
+  })
+  
+  if (postExist) {
+    const getVote = await client.vote.findFirst({
+      where: {
+        feedbackId: req.query.uuid as string,
+        userId: req.session.user?.id
+      }
+    })
+
+    if (!getVote) {
+      // create vote
+      await client.vote.create({
+        data: {
+          userId: req.session.user?.id as string,
+          feedbackId: req.query.uuid as string,
+        }
+      })
+
+      return res.json({ created: true })
+    } else {
+      // remove vote
+      await client.vote.delete({
+        where: {
+          id: getVote.id
+        }
+      })
+
+      return res.json({ deleted: true })
+    }
+  } else {
+    return res.json({
+      _errors: [`Feedback post does not exist by id of ${req.query.uuid}`]
+    })
+  }
+};
 
 export default withSessionRoute((req, res) => {
   if (!req.session.user) return res.status(401).json({ unauthorized: false });
+  const queryID = z.string().uuid().safeParse(req.query.uuid);
+  if (!queryID.success) return res.status(400).json(queryID.error.format());
   if (req.method === "POST") return POST(req, res);
 });
