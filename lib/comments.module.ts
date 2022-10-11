691a6de51c40efcdac6756f6cbf8a274ee901e36
Yofou <exodias1965@gmail.com>
2022-10-11 04:28:28 +0100

    chore(PRO-28): Get all feedback comments endpoint

---
+++ lib/comments.module.ts
@@ -0,0 +1,46 @@
+import { z } from "zod"
+import { client } from "@/prisma/client"
+
+export const ValidateCommentPostInput = z.object({
+  content: z.string().min(25),
+  parentId: z.string().optional(),
+  replyToId: z.string().optional()
+}).refine(async (data) => {
+  if (data.parentId === undefined || data.parentId === null) return true
+  const doesParentCommentExist = await client.comment.count({
+    where: {
+      id: data.parentId
+    }
+  })
+
+  return !!doesParentCommentExist
+}, {
+  message: "Parent comment does not exist"
+}).refine(async (data) => {
+  if (data.replyToId === undefined || data.replyToId === null) return true 
+  if (data.parentId === undefined || data.parentId === null) return false
+
+  const doesReplyCommentToParent = await client.comment.count({
+    where: {
+      id: data.parentId,
+      children: {
+        some: {
+          userId: data.replyToId
+        }
+      }
+    }
+  })
+
+  return !!doesReplyCommentToParent
+}, {
+  message: "replied user has not commented on parent comment"
+})
+
+export const doesPostExist = async (postId?: string) => {
+  if (!postId) return false
+  return !!(await client.feedback.count({
+    where: {
+      id: postId
+    }
+  }))
+}
