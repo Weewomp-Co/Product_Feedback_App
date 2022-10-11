083c89f37e5b0c1d6cb0db480bebd9d60c1ee3f8
Nathan <39814328+Yofou@users.noreply.github.com>
2022-10-11 01:30:18 +0100

    chore(PRO-16): Sign up page (#11)
    * v1.03
    
    * v1.031
    
    * fix(PRO-16): Hook up form with react-hook-forms, zod & uuid
    
    * chore(PRO-16): clean moving page depend styles in /styles folder
    
    * chore(PRO-16): Apply route guarding to sign up & feedback
    
    * chore(PRO-16): Added mobile support for sign up page
    
    Co-authored-by: weewomp1 <103136665+weewomp1@users.noreply.github.com>

--- pages/auth/signup.tsx
+++ pages/auth/signup.tsx
@@ -1,12 +1,143 @@
 import type { NextPage } from "next";
-import AuthContainer from "@/components/shared/layouts/AuthContainer"
+import AuthContainer from "@/components/shared/layouts/AuthContainer";
+import Input from "@/components/shared/input/Input";
+import Button from "@/components/shared/buttons";
+import { inputStyle, InputLabel, Container, ButtonsWrapper, ForgotPassword } from "@/styles/signup"
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { v4 } from "uuid";
+import { useEffect, useState } from "react";
+import { useRouter } from 'next/router'
+import { withSessionSsr } from "@/lib/withSession.module";
+
+const Validation = z
+  .object({
+    username: z.string().min(1).max(8),
+    email: z.string().email(),
+    password: z.string().min(8),
+    confirm: z.string(),
+  })
+  .refine((input) => input.password === input.confirm, {
+    message: "Passwords don't match",
+    path: ["confirm"], // path of error
+  });
 
 const Page: NextPage = () => {
+  const resolver = zodResolver(Validation);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm<z.infer<typeof Validation>>({
+    resolver,
+  });
+
+  const [ids, setIds] = useState<Partial<z.infer<typeof Validation>>>({});
+  useEffect(() => {
+    setIds({
+      username: v4(),
+      email: v4(),
+      password: v4(),
+      confirm: v4(),
+    });
+  }, []);
+
+  const router = useRouter()
+  const onValid = handleSubmit(
+    // on valid
+    async (data) => {
+      const response = await fetch("/api/auth/signup", {
+        method: "POST",
+        body: JSON.stringify({ ...data, confirm_password: data.confirm }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+
+      if (response.ok) {
+        router.push("/feedback")
+      } 
+
+      const result = await response.json()
+      if (result?.email?._errors)
+        setError("email", { message: result.email._errors?.[0] });
+      if (result?.username?._errors)
+        setError("username", { message: result.username._errors?.[0] });
+      if (result?.password?._errors)
+        setError("password", { message: result.password._errors?.[0] });
+      if (result?.confirm_password?._errors)
+        setError("confirm", { message: result.confirm_password._errors?.[0] });
+    }
+  );
+
   return (
-    <AuthContainer title="Sign up" href="/auth/signin">
-      <h1>Hello world</h1>
-    </AuthContainer>
+    <div className={Container()}>
+      <AuthContainer title="Sign up" href="/auth/signin">
+        <form onSubmit={onValid}>
+          <InputLabel htmlFor={ids.username}>Username</InputLabel>
+          <Input
+            id={ids.username}
+            isError={!!errors.username}
+            errorMessage={errors.username?.message ?? ""}
+            type={"text"}
+            css={inputStyle}
+            register={register("username")}
+          />
+          <InputLabel htmlFor={ids.email}>Email</InputLabel>
+          <Input
+            id={ids.email}
+            isError={!!errors.email}
+            errorMessage={errors.email?.message ?? ""}
+            type={"text"}
+            css={inputStyle}
+            register={register("email")}
+          />
+          <InputLabel htmlFor={ids.password}>Password</InputLabel>
+          <Input
+            id={ids.password}
+            isError={!!errors.password}
+            errorMessage={errors.password?.message ?? ""}
+            type="password"
+            css={inputStyle}
+            register={register("password")}
+          />
+          <InputLabel htmlFor={ids.confirm}>Confirm Password</InputLabel>
+          <Input
+            id={ids.confirm}
+            isError={!!errors.confirm}
+            errorMessage={errors.confirm?.message ?? ""}
+            type="password"
+            css={inputStyle}
+            register={register("confirm")}
+          />
+
+          <div className={ButtonsWrapper()}>
+            <Button type="four">Sign up</Button>
+            <Button className={ForgotPassword()} type="five" as="a" href="/">
+              forgot password
+            </Button>
+          </div>
+        </form>
+      </AuthContainer>
+    </div>
   );
 };
+
+export const getServerSideProps = withSessionSsr(({ req }) => {
+  if (req.session.user) {
+    return {
+      redirect: {
+        permanent: true,
+        destination: '/feedback'
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+})
 
 export default Page;
--- stitches.config.ts
+++ stitches.config.ts
@@ -1,6 +1,6 @@
 import { createStitches } from "@stitches/react"
 
-export const { styled, css } = createStitches({
+export const { styled, css, config } = createStitches({
 	theme: {
 		colors: {
 			white300: '#F7F8FD',
@@ -45,5 +45,12 @@ export const { styled, css } = createStitches({
 			h3: '-0.25px',
 			h4: '-0.2px',
 		},
-	}
+  },
+  media: {
+    sm: '(min-width: 640px)',
+    md: '(min-width: 768px)',
+    lg: '(min-width: 1024px)',
+    xl: '(min-width: 1024px)',
+    "2xl": '(min-width: 1536px)'
+  },
 })
