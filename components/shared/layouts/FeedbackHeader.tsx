7300ec669c3948c864b428cb799aed13f69807db
weewomp1 <103136665+weewomp1@users.noreply.github.com>
2022-10-11 20:37:31 -0400

    types and react.fc

--- components/shared/layouts/FeedbackHeader.tsx
+++ components/shared/layouts/FeedbackHeader.tsx
@@ -2,7 +2,15 @@ import React from 'react';
 import { css, styled } from "../../../stitches.config";
 import { FeedbackHeaderStyle, TitleStyle, SubTitleStyle } from './FeedbackHeaderStyle';
 
-const FeedbackHeader = ({title, subtitle}) => {
+type FeedbackHeaderProp = React.PropsWithChildren<{
+  title: string;
+  subtitle: string;
+}>
+
+
+const FeedbackHeader: React.FC<FeedbackHeaderProp> = ({title, subtitle}) =>{
+  
+  
   return (
     <FeedbackHeaderStyle>
       <div className={TitleStyle()}>{title}</div>
