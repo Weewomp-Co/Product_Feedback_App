a6eea7b28888977be55418e23bddf317d46cadda
Yofou <exodias1965@gmail.com>
2022-10-13 00:31:10 +0100

    HOTFIX(PRO-XX): Fix typing issue that failed prod

--- components/shared/layouts/Roadmap.tsx
+++ components/shared/layouts/Roadmap.tsx
@@ -1,21 +1,34 @@
-import React from 'react'
-import {Title, Value, RoadmapContainer, LinkStyle, TitleSection, MainSection, MarkerPlanned, MarkerInProgress, MarkerLive, itemText, itemWrapper} from './RoadmapStyle'
-import Link from 'next/link'
+import React from "react";
+import {
+  Title,
+  Value,
+  RoadmapContainer,
+  LinkStyle,
+  TitleSection,
+  MainSection,
+  MarkerPlanned,
+  MarkerInProgress,
+  MarkerLive,
+  itemText,
+  itemWrapper,
+} from "./RoadmapStyle";
+import Link from "next/link";
 
-const Roadmap = ({Planned, InProgress, Live}) => {
-  
-  type RoadmapContainerProp = React.PropsWithChildren<{
-    Planned: number;
-    InProgress: number;
-    Live: number;
-  }>
+type RoadmapContainerProp = {
+  Planned: number;
+  InProgress: number;
+  Live: number;
+};
 
+const Roadmap: React.FC<RoadmapContainerProp> = ({
+  Planned,
+  InProgress,
+  Live,
+}) => {
   return (
     <RoadmapContainer>
       <div className={TitleSection()}>
-        <div className={Title()}>
-          Roadmap
-        </div>
+        <div className={Title()}>Roadmap</div>
         <div className={LinkStyle()}>
           <Link href="/feedback/roadmap">View</Link>
         </div>
@@ -24,27 +37,42 @@ const Roadmap = ({Planned, InProgress, Live}) => {
         <div className={itemWrapper()}>
           <div className={MarkerPlanned()}></div>
           <div className={itemText()}>Planned</div>
-          <h3 style={{
-            marginLeft: 'auto'
-          }} className={Value()}>{Planned}</h3>
+          <h3
+            style={{
+              marginLeft: "auto",
+            }}
+            className={Value()}
+          >
+            {Planned}
+          </h3>
         </div>
-        <div  className={itemWrapper()}>
+        <div className={itemWrapper()}>
           <div className={MarkerInProgress()}></div>
           <div className={itemText()}>In-Progress</div>
-          <h3 style={{
-            marginLeft: 'auto'
-          }} className={Value()}>{InProgress}</h3>
+          <h3
+            style={{
+              marginLeft: "auto",
+            }}
+            className={Value()}
+          >
+            {InProgress}
+          </h3>
         </div>
         <div className={itemWrapper()}>
           <div className={MarkerLive()}></div>
           <div className={itemText()}>Live</div>
-          <h3 style={{
-            marginLeft: 'auto'
-          }} className={Value()}>{Live}</h3>
+          <h3
+            style={{
+              marginLeft: "auto",
+            }}
+            className={Value()}
+          >
+            {Live}
+          </h3>
         </div>
       </div>
     </RoadmapContainer>
-  )
-}
+  );
+};
 
-export default Roadmap
+export default Roadmap;
