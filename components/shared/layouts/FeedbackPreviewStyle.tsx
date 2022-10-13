608cad72c30508680aafa98db7442434359029f7
weewomp1 <103136665+weewomp1@users.noreply.github.com>
2022-10-12 21:49:20 -0400

    PRO-34 v1.2

--- components/shared/layouts/FeedbackPreviewStyle.tsx
+++ components/shared/layouts/FeedbackPreviewStyle.tsx
@@ -2,5 +2,14 @@ import { styled, css } from "stitches.config"
 
 
 export const FeedbackPreviewContainer = styled('div', {
-  
-})
+  fontFamily: 'jost',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'start',
+  minHeight: '9.438rem',
+  maxHeight: '9.438rem',
+  maxWidth: '51.563rem',
+  padding: '2rem',
+  borderRadius: '0.625rem',
+  position: 'relative'
+})  
