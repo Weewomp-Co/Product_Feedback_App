74f9586edc2fbff0c81468edc028c2708a308b2e
weewomp1 <103136665+weewomp1@users.noreply.github.com>
2022-10-12 22:26:16 -0400

    PRO-34 HOTFIX
    Refactored styles, Removed comments, added p tags

--- components/shared/layouts/FeedbackPreview.tsx
+++ components/shared/layouts/FeedbackPreview.tsx
@@ -1,80 +1,12 @@
 import React from 'react'
 import Votes from '../votes/Votes'
-import {FeedbackPreviewContainer} from './FeedbackPreviewStyle'
+import {FeedbackPreviewContainer, title, CommentsStyle, subtitle, textWrapper, VotesStyle, tag} from './FeedbackPreviewStyle'
 import {useState, useEffect, Dispatch} from 'react'
 import { css, styled } from "../../../stitches.config";
 import { Comments } from '@/assets/comments';
 import Image from 'next/image';
 import Link from 'next/link';
 
-const title = css({
-  fontWeight: 'bold',
-  color: '$grey600',
-  fontSize: '$h3',
-  '&::after': {
-    position: 'absolute',
-    inset: '0',
-    content: '',
-    display: 'block'
-  },
-  '&:hover': {
-    color: '$grey900'
-  }
-})
-
-const titleHover = css({
-  fontWeight: 'bold',
-  color: '$grey900',
-  fontSize: '$h3',
-})
-
-const CommentsStyle = css({
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  marginLeft: 'auto',
-  marginTop: 'auto',
-  marginBottom: 'auto',
-  gap: '1rem',
-  fontWeight: 'bold'
-})
-
-const subtitle = css({
-  fontSize: '$body1',
-  color: '$grey300'
-})
-
-const textWrapper = css({
-  display: 'flex',
-  justifyContent: 'start',
-  alignItems: 'start',
-  flexDirection: 'column',
-  paddingLeft: '2.5rem',
-  gap: '0.75rem'
-})
-
-const textWrapperHover = css({
-  display: 'flex',
-  justifyContent: 'start',
-  alignItems: 'start',
-  flexDirection: 'column',
-  paddingLeft: '2.5rem',
-  gap: '0.75rem',
-  color: '$grey900'
-})
-
-const VotesStyle = css({  
-  zIndex: '1'
-})
-
-const tag = css({
-  color: '$grey900',
-  fontSize: '$body3',
-  backgroundColor: '$white600',
-  padding: '0.375rem 1rem',
-  borderRadius: '0.625rem',
-  fontWeight: '$semibold'
-})
 
 type FeedbackPreviewProp = React.PropsWithChildren<{
   votes: number;
@@ -99,30 +31,26 @@ const FeedbackPreview: React.FC<FeedbackPreviewProp> = ({
   setActive,
   uuid
 }) => {
-  // const [votes, setVotes] = useState(30);
-  // const [active, setActive] = useState(false)
-
   const [hovering, setHovering] = useState(false)
 
   return (
     <FeedbackPreviewContainer onMouseEnter={() => {
       setHovering(true);
-      console.log('entered')
     }} onMouseLeave={() => {
       setHovering(false)
     }}>
         <Votes votes={votes} setVotes={setVotes} active={active} setActive={setActive} className={VotesStyle()}/>
         <div className={textWrapper()}>     
           <Link href={`/feedback/${uuid}`}><a className={title()}>Add tags for solutions</a></Link>
-          <div className={subtitle()}>Easier to search for solutions based on a specific stack.</div>
-          <div className={tag()}>
+          <p className={subtitle()}>Easier to search for solutions based on a specific stack.</p>
+          <p className={tag()}>
           {Category}
-          </div>
+          </p>
         </div>
        
-        <div className={CommentsStyle()}>
+        <p className={CommentsStyle()}>
           <Comments /> {commentsNumber}
-        </div>
+        </p>
     </FeedbackPreviewContainer>
   )
 }
--- components/shared/layouts/FeedbackPreviewStyle.tsx
+++ components/shared/layouts/FeedbackPreviewStyle.tsx
@@ -13,3 +13,59 @@ export const FeedbackPreviewContainer = styled('div', {
   borderRadius: '0.625rem',
   position: 'relative'
 })  
+
+export const title = css({
+  fontWeight: 'bold',
+  color: '$grey600',
+  fontSize: '$h3',
+  '&::after': {
+    position: 'absolute',
+    inset: '0',
+    content: '',
+    display: 'block'
+  },
+  '&:hover': {
+    color: '$grey900'
+  }
+})
+
+export const CommentsStyle = css({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  marginLeft: 'auto',
+  marginTop: 'auto',
+  marginBottom: 'auto',
+  gap: '1rem',
+  fontWeight: 'bold',
+  marginRight: '0'
+})
+
+export const subtitle = css({
+  fontSize: '$body1',
+  color: '$grey300',
+  margin: '0'
+})
+
+export const textWrapper = css({
+  display: 'flex',
+  justifyContent: 'start',
+  alignItems: 'start',
+  flexDirection: 'column',
+  paddingLeft: '2.5rem',
+  gap: '0.75rem'
+})
+
+export const VotesStyle = css({  
+  zIndex: '1'
+})
+
+export const tag = css({
+  color: '$grey900',
+  fontSize: '$body3',
+  backgroundColor: '$white600',
+  padding: '0.375rem 1rem',
+  borderRadius: '0.625rem',
+  fontWeight: '$semibold',
+  margin: '0'
+})
