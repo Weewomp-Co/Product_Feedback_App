8d67ffc7817b34457a574dd88d7068afd25d94b7
weewomp1 <103136665+weewomp1@users.noreply.github.com>
2022-10-15 23:31:23 -0400

    Changes
    changed button css variant "type" to "color" to avoid conflicting with the type HTML property on button, updated all references
    
    adjusted padding

--- components/shared/buttons/index.tsx
+++ components/shared/buttons/index.tsx
@@ -1,7 +1,7 @@
 import { styled } from '../../../stitches.config'
 
 export default styled('button', {
-  padding: ".75em 3.125em",
+  padding: ".75rem 1.5rem",
   fontFamily: '$jost',
   fontSize: '$h4',
   fontWeight: '$bold',
@@ -11,7 +11,7 @@ export default styled('button', {
   borderRadius: '10px',
   color: '$white300',
   variants: {
-    type: {
+    color: {
       one: {
         backgroundColor: '$purple',
         "&:hover": {
@@ -46,6 +46,6 @@ export default styled('button', {
     },
   },
   defaultVariants: {
-    type: 'one'
+    color: 'one'
   }
 });
