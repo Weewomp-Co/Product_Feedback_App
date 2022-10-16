7539d8d28eeef278d7bf4132b3bffaa46765f1fe
Yofou <exodias1965@gmail.com>
2022-10-16 06:12:19 +0100

    HOTFIX(PRO-XXX): Make sure to hit production endpoints with env

--- lib/stores.ts
+++ lib/stores.ts
@@ -6,7 +6,8 @@ type AuthMeResponse = ReturnType<typeof sanitizeUser>;
 export const userAtom = atomWithQuery<AuthMeResponse, AuthMeResponse>(() => ({
   queryKey: ["auth me"],
   queryFn: async () => {
-    const res = await fetch(`http://localhost:3000/api/auth/me`);
+    const host = process.env.NEXT_PUBLIC_HOSTNAME ?? 'http://localhost:3000'
+    const res = await fetch(`${host}/api/auth/me`);
     return res.json();
   },
 }));
