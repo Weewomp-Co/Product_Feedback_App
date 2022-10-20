8441aad21e95167c8702ff1a7ae0ee16d901ad4c
Yofou <exodias1965@gmail.com>
2022-10-20 03:05:18 +0100

    fix(PRO-36): change stop-color to stopColor

--- assets/Plus.tsx
+++ assets/Plus.tsx
@@ -10,9 +10,9 @@ export const Plus: React.FC<PlusProps> = ({ className="" }) => {
     <path d="M30.3425 36V30.1657H36.0295V25.8637H30.3425V20H25.7459V25.8637H20V30.1657H25.7459V36H30.3425Z" fill="white"/>
     <defs>
     <radialGradient id="paint0_radial_0_2017" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(58.184 -5.81647) rotate(129.411) scale(93.4169)">
-    <stop stop-color="#E84D70"/>
-    <stop offset="0.530886" stop-color="#A337F6"/>
-    <stop offset="1" stop-color="#28A7ED"/>
+    <stop stopColor="#E84D70"/>
+    <stop offset="0.530886" stopColor="#A337F6"/>
+    <stop offset="1" stopColor="#28A7ED"/>
     </radialGradient>
     </defs>
     </svg>
