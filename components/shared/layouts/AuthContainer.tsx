9e6b0d547275383c4818bd7411a306359f70a703
Yofou <exodias1965@gmail.com>
2022-10-20 07:00:44 +0100

    fix(PRO-60): Make sure go back button go back in history for sign in

--- components/shared/layouts/AuthContainer.tsx
+++ components/shared/layouts/AuthContainer.tsx
@@ -1,36 +1,52 @@
-import { AuthContainerStyle, TitleSection, Title, Container, TopContainer, BackButton, InnerSection  } from "./AuthContainerStyle";
+import {
+  AuthContainerStyle,
+  TitleSection,
+  Title,
+  Container,
+  TopContainer,
+  BackButton,
+  InnerSection,
+} from "./AuthContainerStyle";
 import { BackArrow } from "@/assets/backArrow";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 type AuthContainerProp = React.PropsWithChildren<{
   title: string;
-  href: string;
-  className?: string; 
-}>
-
+  href?: string;
+  className?: string;
+}>;
 
 const AuthContainer: React.FC<AuthContainerProp> = ({
   title,
   href,
   children,
-  className=""
+  className = "",
 }) => {
+  const router = useRouter()
+  const onGoBack = () => {
+    router.back()
+  }
   return (
     <main className={TopContainer()}>
       <InnerSection>
-        <Link href={href}>
-          <BackButton as="a" color="three">
+        {href ? (
+          <Link href={href}>
+            <BackButton as="a" color="three">
+              <BackArrow /> Go Back
+            </BackButton>
+          </Link>
+        ) : (
+          <BackButton onClick={onGoBack} color="three">
             <BackArrow /> Go Back
           </BackButton>
-        </Link>
+        )}
         <Container className={className}>
           <TitleSection>
             <h2 className={Title()}>{title}</h2>
           </TitleSection>
 
-          <AuthContainerStyle>
-            {children}
-          </AuthContainerStyle>
+          <AuthContainerStyle>{children}</AuthContainerStyle>
         </Container>
       </InnerSection>
     </main>
