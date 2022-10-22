d35af5fee54f4e2281f6d8e2abcf9a35495b8112
weewomp1 <103136665+weewomp1@users.noreply.github.com>
2022-10-22 18:33:36 -0400

    Pro 49/roadmap page (#41)
    * (PRO-49) Votes Button Styled
    
    * PRO-46(DESKTOP STYLING)
    
    * Update roadmap.tsx
    
    * Update roadmap.tsx
    
    * PRO-48/STLYING AND LINKS
    
    * PRO-49/ROADMAP styling
    
    mobile styling adjust
    
    * PRO-49/PR
    
    * (PRO-49)/removed pointless files
    
    * PRO-49/REFACTORING-STYLES
    
    * (PRO-49)StatusToColor, section tag
    
    * (PRO-49) - CLEANUP
    
    Add feedback button
    refactor styles
    remove spaces
    used h2
    
    * (PRO-49) - CLEANUP
    
    use CSS<typeof config>
    
    removed unused imports etc
    
    * (PRO-49) - FORMAT
    
    prettier roadmap.tsx and .ts
    
    font familty 'jost' added to LabelStyle()
    
    * chore(PRO-49): Some clean up with CSS and Column Container
    
    Co-authored-by: Yofou <exodias1965@gmail.com>

---
+++ components/roadmap/roadmap-page-card/index.tsx
@@ -0,0 +1,82 @@
+import { useAtom} from "jotai";
+import React from "react";
+import {Dot, RoadmapPageCardContainer, Container, Type, Title, Desc, Category, LinkStyle, StyledVotes , VotesSection, CommentsWrapper, ContentWrapper} from './styles'
+import Votes from '@/components/shared/votes/Votes'
+import {Comments} from '@/assets/comments'
+import Link from 'next/link'
+import { queryClientAtom } from "jotai/query";
+import {userAtom} from '@/lib/stores'
+
+type RoadmapPageCardProps = {
+  status: string;
+  title?: string;
+  desc?: string;
+  category?: string;
+  votes: number;
+  commentsNumber?: number;
+  uuid?: string
+  active: boolean
+}
+
+const RoadmapPageCard: React.FC<RoadmapPageCardProps> = ({
+  status,
+  title,
+  desc,
+  category,
+  votes,
+  commentsNumber,
+  uuid,
+  active
+}) => {
+  const [queryClient] = useAtom(queryClientAtom)
+  const [_, dispatch] = useAtom(userAtom)
+
+  const StatusToColor: Record<string, string> = {
+    'Planned': '#F49F85',
+    'Progress': '#AD1FEA',
+    'Live': '#62BCFA'
+  }
+
+  const onVote = async () => {
+    await fetch(`/api/feedback/${uuid}/votes`, {
+      method: 'POST'
+    })
+
+    queryClient.invalidateQueries(['feedbacks'])
+    dispatch({ type: 'refetch' })
+  }
+
+  return (
+    <RoadmapPageCardContainer css={{
+      borderTop: `0.375rem solid ${StatusToColor[status] ?? 'transparent'}`,
+    }}>
+      
+      <div>
+        <Container>
+          <Dot css={{
+            backgroundColor: `${StatusToColor[status] ?? 'transparent'}`
+          }}/>
+          <div className={Type()}>{status}</div>
+        </Container>
+        <div className={Title()}>
+          <Link href={`/feedback/${uuid}`}><a className={LinkStyle()}>{title}</a></Link>
+        </div>
+        <p className={Desc()}>{desc}</p>
+      </div>
+      <div className={VotesSection()}>
+        <Category>{category}</Category>
+        <div className={ContentWrapper()}>
+          <Votes votes={votes} active={active} css={StyledVotes} onClick={onVote}/>
+          <div className={CommentsWrapper()}>
+            <Comments />
+            <h4 style={{
+              margin: '0'
+            }}>{commentsNumber}</h4>
+          </div>
+        </div>
+      </div>
+    </RoadmapPageCardContainer>
+  )
+}
+
+export default RoadmapPageCard
